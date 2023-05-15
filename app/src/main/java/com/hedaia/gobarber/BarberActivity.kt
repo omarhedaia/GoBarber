56990fbe14d01177f081a70398f8f4a6fa5638e6
Omar-Hedaia-codingdojo <ohedaia@codingdojo.com>
2023-05-15 11:26:56 +0300

    UI Commit

---
+++ app/src/main/java/com/hedaia/gobarber/BarberActivity.kt
@@ -0,0 +1,11 @@
+package com.hedaia.gobarber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BarberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barber)
+    }
+}
