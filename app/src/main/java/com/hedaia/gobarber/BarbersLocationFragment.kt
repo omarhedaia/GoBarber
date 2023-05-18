c90168f1c86661563960ff5a7a9f2acf4e84780c
Omar-Hedaia-codingdojo <ohedaia@codingdojo.com>
2023-05-18 03:26:12 +0300

    Adding layout for the serviceProvider info and showing it on map

--- app/src/main/java/com/hedaia/gobarber/BarbersLocationFragment.kt
+++ app/src/main/java/com/hedaia/gobarber/BarbersLocationFragment.kt
@@ -1,5 +1,7 @@
 package com.hedaia.gobarber
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.util.Log
@@ -10,10 +12,12 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.testgolden.Views.Adapters.ServicesAdapter
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.common.GooglePlayServicesUtil.isGooglePlayServicesAvailable
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -25,29 +29,24 @@ import com.hedaia.gobarber.Models.Services
 import com.hedaia.gobarber.ViewModels.CustomersViewModel
 import com.hedaia.gobarber.Views.BarbersAdapter
 import com.hedaia.gobarber.Views.ServicesProvidersAdapter
+import com.hedaia.gobarber.databinding.BarberInfoMarkerBinding
 import com.hedaia.gobarber.databinding.FragmentBarbersLocationBinding
 
-class BarbersLocationFragment : Fragment(),ServicesProvidersAdapter.onClick,OnMapReadyCallback {
+class BarbersLocationFragment : Fragment(),ServicesProvidersAdapter.onClick,OnMapReadyCallback,
+    OnMapsSdkInitializedCallback {
 
     lateinit var binding: FragmentBarbersLocationBinding
     lateinit var viewModel: CustomersViewModel
     var serviceProviders= arrayListOf<ServiceProvider>()
     lateinit var servicesProvidersAdapter: ServicesProvidersAdapter
+    lateinit var currentGoogleMap:GoogleMap
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MapsInitializer.initialize(requireContext(),MapsInitializer.Renderer.LATEST,this)
 
+    }
 
-//    private val callback = OnMapReadyCallback { googleMap ->
-//        /**
-//         * Manipulates the map once available.
-//         * This callback is triggered when the map is ready to be used.
-//         * This is where we can add markers or lines, add listeners or move the camera.
-//         * In this case, we just add a marker near Sydney, Australia.
-//         * If Google Play services is not installed on the device, the user will be prompted to
-//         * install it inside the SupportMapFragment. This method will only be triggered once the
-//         * user has installed Google Play services and returned to the app.
-//         */
-//
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,6 +63,34 @@ class BarbersLocationFragment : Fragment(),ServicesProvidersAdapter.onClick,OnMa
             serviceProviders.addAll(it)
             Log.d("TAG", "onCreate: $serviceProviders")
             servicesProvidersAdapter.updateList(serviceProviders)
+            if (currentGoogleMap!=null)
+            {
+                for (serviceProvider in serviceProviders)
+                {
+                    Log.d("markerLocation", "onViewCreated: ${serviceProvider.name}")
+
+                    val serviceProviderMarkerBinding = BarberInfoMarkerBinding.inflate(layoutInflater)
+
+                    serviceProviderMarkerBinding.apply {
+
+                        barberTitleTv.text = serviceProvider.name
+                        barberDistanceTv.text = "20km"
+                        barberImageIv.setImageResource(R.drawable.afro)
+                        val latitude = serviceProvider.latitude
+                        Log.d("markerLocation", "onMapReady: latitude = $latitude ")
+                        val longitude = serviceProvider.longitude
+                        Log.d("markerLocation", "onMapReady: longitude = $longitude ")
+
+                        val location = LatLng(latitude!!.toDouble(),longitude!!.toDouble())
+                val barberIcon = bitmapToSmallMarker(serviceProviderMarkerBinding.root)
+                        currentGoogleMap.addMarker(MarkerOptions().position(location).title(serviceProvider.name).icon(barberIcon))
+                        currentGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+                    }
+
+
+
+                }
+            }
         }
 
         binding.apply {
@@ -73,6 +100,9 @@ class BarbersLocationFragment : Fragment(),ServicesProvidersAdapter.onClick,OnMa
 
 
         }
+        val googleApistatues = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(requireContext())
+
+
 
         return binding.root
     }
@@ -81,6 +111,15 @@ class BarbersLocationFragment : Fragment(),ServicesProvidersAdapter.onClick,OnMa
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+
     }
 
     override fun setserviceProvider(serviceProvider: ServiceProvider) {
@@ -90,10 +129,46 @@ class BarbersLocationFragment : Fragment(),ServicesProvidersAdapter.onClick,OnMa
 
     override fun onMapReady(googleMap: GoogleMap) {
 
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        currentGoogleMap = googleMap
 
+
+
+
+
+//        val sydney = LatLng(-34.0, 151.0)
+//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
+    }
+
+    fun bitmapToSmallMarker(view: View):BitmapDescriptor{
+        val bitmap = Bitmap.createScaledBitmap(
+            viewToBitmap(view),
+            view.width,
+            view.height,
+            false)
+
+        val smallMarker = BitmapDescriptorFactory.fromBitmap(bitmap)
+        return smallMarker
+
+    }
+
+    fun viewToBitmap(view:View):Bitmap{
+
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val bitmap = Bitmap.createBitmap(view.measuredWidth,view.measuredHeight,Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.layout(0,0,view.measuredWidth,view.measuredHeight)
+        view.draw(canvas)
+        return bitmap
+    }
+
+    override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
+        when (renderer) {
+            MapsInitializer.Renderer.LATEST -> Log.d("MapsDemo", "The latest version of the renderer is used.")
+            MapsInitializer.Renderer.LEGACY -> Log.d("MapsDemo", "The legacy version of the renderer is used.")
+        }
     }
 
 
